e39cfa6c8de368d397eefed0dcd5d166e4acfab6
suoServe <1354246210@qq.com>
2019-02-12 14:53:57 +0800

    update

--- MapToolServe/out/Main.js
+++ MapToolServe/out/Main.js
@@ -3,7 +3,7 @@ var http = require("http");
 //设置主机名
 var hostName = '127.0.0.1';
 //设置端口
-var port = 8080;
+var port = 4000;
 //创建服务
 var server = http.createServer(function (req, res) {
     res.setHeader('Content-Type', 'text/plain');
@@ -11,7 +11,7 @@ var server = http.createServer(function (req, res) {
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.end("hello nodejs");
     console.log(req, res);
-}).listen(8888);
+});
 server.listen(port, hostName, function () {
     console.log(`服务器运行在http://${hostName}:${port}`);
 });
